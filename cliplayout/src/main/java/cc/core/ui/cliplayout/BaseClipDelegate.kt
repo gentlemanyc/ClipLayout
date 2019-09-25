0ce43581e6c04e471850c88d83da44396539f275
yuanc <yuanchao>
2019-09-25 09:41:16 +0800

    优化ClipFrameLayout

---
+++ cliplayout/src/main/java/cc/core/ui/cliplayout/BaseClipDelegate.kt
@@ -0,0 +1,118 @@
+package cc.core.ui.cliplayout
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * All rights Reserved, Designed By yuanchao
+ * @projectName ClipLayout
+ * @package   cc.core.ui.cliplayout
+ * @author 袁超
+ * @date  19-9-25上午9:08
+ */
+class BaseClipDelegate {
+    var pathList = mutableListOf<Pair<View, Path>>()
+    var bgPath = Path()
+    var bgPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    var maskColor = Color.BLACK
+    val mode = PorterDuffXfermode(PorterDuff.Mode.SRC_OUT)
+    var ignoreAll = false
+
+    fun init(context: Context, attrs: AttributeSet?) {
+        if (attrs != null) {
+            val ta = context.obtainStyledAttributes(attrs, R.styleable.ClipShapeFrameLayout)
+            maskColor = ta.getColor(R.styleable.ClipShapeFrameLayout_clip_mask_color, Color.BLACK)
+            ignoreAll = ta.getBoolean(R.styleable.ClipShapeFrameLayout_ignore_all_clip, false)
+            ta.recycle()
+        }
+        bgPaint.color = maskColor
+    }
+
+    fun onMeasure(view: View) {
+        bgPath.reset()
+        bgPath.addRect(
+            0F,
+            0F,
+            view.measuredWidth.toFloat(),
+            view.measuredHeight.toFloat(),
+            Path.Direction.CCW
+        )
+    }
+
+    fun onLayout(viewGroup: ViewGroup) {
+        if (pathList.isEmpty()) {
+            for (i in 0 until viewGroup.childCount) {
+                val child = viewGroup.getChildAt(i)
+                val lp = child.layoutParams as ClipLayoutParams
+                if (lp.ignoreClip()) {
+                    continue
+                }
+                val radius = lp.radius()
+
+                val path = Path()
+                val rectLeft = child.left.toFloat()
+                val rectRight = child.top.toFloat()
+
+                val rect =
+                    RectF(
+                        rectLeft,
+                        rectRight,
+                        rectLeft + child.measuredWidth.toFloat(),
+                        rectRight + child.measuredHeight.toFloat()
+                    )
+
+                if (radius == 0F) {
+                    path.addRect(rect, Path.Direction.CCW)
+                } else {
+                    path.addRoundRect(
+                        rect,
+                        floatArrayOf(
+                            radius, radius, radius, radius, radius, radius, radius, radius
+                        ),
+                        Path.Direction.CCW
+                    )
+                }
+                pathList.add(Pair(child, path))
+            }
+        }
+    }
+
+    fun draw(viewGroup: ViewGroup, canvas: Canvas) {
+        if (ignoreAll) {
+            return
+        }
+        if (viewGroup.isInEditMode) {
+            pathList.forEachIndexed { index, pair ->
+                if (pair.first.visibility == View.VISIBLE) {
+                    canvas.clipPath(pair.second, Region.Op.DIFFERENCE)
+                }
+            }
+            canvas.drawColor(maskColor)
+        } else {
+            viewGroup.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+            canvas.save()
+            canvas.drawRect(
+                viewGroup.left.toFloat(),
+                viewGroup.top.toFloat(),
+                viewGroup.right.toFloat(),
+                viewGroup.bottom.toFloat(),
+                bgPaint
+            )
+            bgPaint.xfermode = mode
+            pathList.forEachIndexed { index, pair ->
+                if (pair.first.visibility != View.GONE) {
+                    canvas.drawPath(pair.second, bgPaint)
+                }
+            }
+            bgPaint.xfermode = null
+        }
+    }
+}
+
+interface ClipLayoutParams {
+    fun radius(): Float
+    fun ignoreClip(): Boolean
+}
--- cliplayout/src/main/java/cc/core/ui/cliplayout/ClipShapeFrameLayout.kt
+++ cliplayout/src/main/java/cc/core/ui/cliplayout/ClipShapeFrameLayout.kt
@@ -8,22 +8,15 @@ import android.os.Build
 import android.util.AttributeSet
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import android.graphics.Paint.FILTER_BITMAP_FLAG
-import android.graphics.Paint.ANTI_ALIAS_FLAG
-import android.graphics.PaintFlagsDrawFilter
-import android.graphics.Canvas.ALL_SAVE_FLAG
-import android.view.View
 
 
 class ClipShapeFrameLayout : FrameLayout {
-    private var pathList = mutableListOf<Path>()
-    private var bgPath = Path()
-    private var bgPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private var maskColor = Color.BLACK
+
+    val clipDelegate = BaseClipDelegate()
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        init(context, attrs)
+        clipDelegate.init(context, attrs)
     }
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
@@ -31,114 +24,46 @@ class ClipShapeFrameLayout : FrameLayout {
         attrs,
         defStyleAttr
     ) {
-        init(context, attrs)
+        clipDelegate.init(context, attrs)
     }
 
-    private fun init(context: Context, attrs: AttributeSet?) {
-        if (attrs != null) {
-            val ta = context.obtainStyledAttributes(attrs, R.styleable.ClipShapeFrameLayout)
-            maskColor = ta.getColor(R.styleable.ClipShapeFrameLayout_clip_mask_color, Color.BLACK)
-            ta.recycle()
-        }
-        bgPaint.color = maskColor
-    }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        bgPath.addRect(
-            0F,
-            0F,
-            measuredWidth.toFloat(),
-            measuredHeight.toFloat(),
-            Path.Direction.CCW
-        )
+        clipDelegate.onMeasure(this)
     }
 
     @SuppressLint("DrawAllocation")
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
-        if (pathList.isEmpty()) {
-            for (i in 0 until childCount) {
-
-                val child = getChildAt(i)
-                val lp = child.layoutParams as LayoutParams
-                if (lp.notClip) {
-                    continue
-                }
-                val radius = lp.radius
-
-                val path = Path()
-                val rectLeft = child.left.toFloat()
-                val rectRight = child.top.toFloat()
-
-                val rect =
-                    RectF(
-                        rectLeft,
-                        rectRight,
-                        rectLeft + child.measuredWidth.toFloat(),
-                        rectRight + child.measuredHeight.toFloat()
-                    )
-
-                if (radius == 0F) {
-                    path.addRect(rect, Path.Direction.CCW)
-                } else {
-                    path.addRoundRect(
-                        rect,
-                        floatArrayOf(
-                            radius, radius, radius, radius, radius, radius, radius, radius
-                        ),
-                        Path.Direction.CCW
-                    )
-                }
-                pathList.add(path)
-            }
-        }
+        clipDelegate.onLayout(this)
     }
 
-    val mode = PorterDuffXfermode(PorterDuff.Mode.SRC_OUT)
     override fun dispatchDraw(canvas: Canvas) {
-        if (isInEditMode) {
-            pathList.forEachIndexed { index, path ->
-                canvas.clipPath(path, Region.Op.DIFFERENCE)
-            }
-            canvas.drawColor(maskColor)
-        } else {
-            setLayerType(View.LAYER_TYPE_SOFTWARE, null)
-            canvas.save()
-            canvas.drawRect(
-                left.toFloat(),
-                top.toFloat(),
-                right.toFloat(),
-                bottom.toFloat(),
-                bgPaint
-            )
-            bgPaint.xfermode = mode
-            pathList.forEachIndexed { index, path ->
-                canvas.drawPath(path, bgPaint)
-            }
-            bgPaint.xfermode = null
-        }
+        clipDelegate.draw(this, canvas)
         super.dispatchDraw(canvas)
-    }
-
-    override fun generateDefaultLayoutParams(): LayoutParams {
-        return LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
-        )
     }
 
     override fun generateLayoutParams(attrs: AttributeSet?): FrameLayout.LayoutParams {
         return LayoutParams(context, attrs)
     }
 
-    class LayoutParams : FrameLayout.LayoutParams {
+    class LayoutParams : FrameLayout.LayoutParams, ClipLayoutParams {
+        override fun radius(): Float {
+            return radius
+        }
+
+        override fun ignoreClip(): Boolean {
+            return ignoreClip
+        }
+
         @SuppressLint("CustomViewStyleable")
         constructor(c: Context, attrs: AttributeSet?) : super(c, attrs) {
             if (attrs != null) {
                 val ta = c.obtainStyledAttributes(attrs, R.styleable.ClipShapeFrameLayout)
                 radius = ta.getDimension(R.styleable.ClipShapeFrameLayout_layout_shape_radius, 0F)
-                notClip = ta.getBoolean(R.styleable.ClipShapeFrameLayout_layout_not_clip, false)
+                ignoreClip =
+                    ta.getBoolean(R.styleable.ClipShapeFrameLayout_layout_ignore_clip, false)
                 ta.recycle()
             }
         }
@@ -154,6 +79,6 @@ class ClipShapeFrameLayout : FrameLayout {
         /**
          * 不剪裁，默认都裁剪。
          */
-        var notClip = false
+        var ignoreClip = false
     }
 }
